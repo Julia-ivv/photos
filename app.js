dc088f796ea89a68684f46cdb1784ea56fa074f0
Julia Ivanova <julia_sji85@mail.ru>
2020-06-29 18:43:47 +0300

    connection to mongo

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const usersRouter = require('./routes/users.js');
 const cardsRouter = require('./routes/cards.js');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
