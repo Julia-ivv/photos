bbc3f151b8c5494d25f27214518cbe9f5f8411bb
Julia Ivanova <julia_sji85@mail.ru>
2020-06-29 23:51:25 +0300

    created schemes and models

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.path('avatar').validate((val) => {
+  const urlRegex = /^https?:\/\/(www\.)?(((\d{1,3}\.){3}\d{1,3})|([А-ЯЁа-яё0-9][0-9А-ЯЁа-яё\-\.]*\.[А-ЯЁа-яё]+|[a-zA-Z0-9][a-zA-Z0-9\-\.]*\.[a-zA-Z]+))(:[1-9]\d{1,4})?\/?([0-9/a-zA-Z]+#?)?$/;
+  return urlRegex.test(val);
+}, 'Неверный URL');
+
+module.exports = mongoose.model('user', userSchema);
